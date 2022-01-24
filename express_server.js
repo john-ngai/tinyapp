6fb9c12ddcc9efa6a4723aee742a8e2064f262ea
mrjohnming <john.ngai@hotmail.com>
2022-01-24 20:26:30 +0000

    Removed unnecessary lines

--- express_server.js
+++ express_server.js
@@ -17,13 +17,14 @@ app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n');
-});
-
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  res.render("urls_show", templateVars);
 });
 
 app.listen(port, () => {
