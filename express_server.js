dd047f2f3094cea7c871538598299e2ab391dd4e
mrjohnming <john.ngai@hotmail.com>
2022-01-26 19:00:03 +0000

    Added a GET & POST for /register

--- express_server.js
+++ express_server.js
@@ -4,6 +4,8 @@ const cookieParser = require('cookie-parser');
 const app = express();
 const port = 8080;
 
+const { hexNumGenerator } = require('./exports/hexNumGenerator');
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
@@ -12,10 +14,23 @@ const generateRandomString = () => {
   return Math.random().toString(36).slice(2, 8);
 }
 
+const users = {
+  'userRandomID': {
+    id: 'userRandomID',
+    email: 'user@example.com',
+    password: 'purple-monkey-dinosaur'
+  },
+  'user2RandomID': {
+    id: 'user2RandomID',
+    email: 'user2@example.com',
+    password: 'dishwasher-funk',
+  },
+};
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
-}
+};
 
 app.get('/', (req, res) => {
   res.redirect('/urls');
@@ -83,6 +98,23 @@ app.post('/login', (req, res) => {
 
 app.post('/logout', (req, res) => {
   res.clearCookie('username');
+  res.redirect('/urls');
+});
+
+app.get('/register', (req, res) => {
+  res.render('urls_register');
+});
+
+app.post('/register', (req, res) => {
+  const newUserID = hexNumGenerator(6);
+  const newEmail = req.body.email;
+  const newPassword = req.body.password;
+  users.newUserID = {
+    id: newUserID,
+    email: newEmail,
+    password: newPassword,
+  }
+  res.cookie('user_id', newUserID);
   res.redirect('/urls');
 });
 
