099ff3e4efe67dc6b7737fbfd289d4d9e53f541f
mrjohnming <john.ngai@hotmail.com>
2022-01-27 13:45:19 +0000

    Corrected the ./function_modules import folder name

--- express_server.js
+++ express_server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
 const app = express();
 const port = 8080;
 
-const { hexNumGenerator } = require('./exports/hexNumGenerator');
-const { emailLookup, passwordLookup, userIDLookup, urlsForUser, urlOwner } = require('./exports/userDataLookup');
+const { hexNumGenerator } = require('./function_modules/hexNumGenerator');
+const { emailLookup, passwordLookup, userIDLookup, urlsForUser, urlOwner } = require('./function_modules/userDataLookup');
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -42,6 +43,8 @@ app.get('/', (req, res) => {
 
 
 app.get('/urls', (req, res) => {
+  console.log(users) // TEST CASE TEST CASE TEST CASE
+
   const userIDCookie = req.cookies.user_id;
   const myURLs = urlsForUser(urlDatabase, userIDCookie);
   const templateVars = {
@@ -142,12 +145,16 @@ app.post('/urls/:shortURL', (req, res) => {
 app.post('/login', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
+
   if (!emailLookup(users, email)) {
     return res.status(403).send(`ERROR (403): ${email} is not a registered email.`);
   }
+  
+  // passwordLookup is OUTDATED
   if (!passwordLookup(users, email, password)) {
     return res.status(403).send('ERROR (403): Incorrect password. Please try again.');
   }
+  
   const id = userIDLookup(users, email);
   res.cookie('user_id', id);
   res.redirect('/urls');
@@ -164,11 +171,12 @@ app.get('/register', (req, res) => {
   res.render('urls_register');
 });
 
-
+///////////////////////////////////
 app.post('/register', (req, res) => {
   const newUserID = hexNumGenerator(6);
   const newEmail = req.body.email;
   const newPassword = req.body.password;
+  const hashedPassword = bcrypt.hashSync(password, 10);
 
   if (newEmail === '' || newPassword === '') {
     return res.status(400).send('ERROR (400): Empty email and/or password field.');
@@ -179,12 +187,12 @@ app.post('/register', (req, res) => {
   users[newUserID] = {
     id: newUserID,
     email: newEmail,
-    password: newPassword,
+    password: hashedPassword,
   };
   res.cookie('user_id', newUserID);
   res.redirect('/urls');
 });
-
+///////////////////////////////////
 
 app.get('/login', (req, res) => {
   res.render('urls_login');
