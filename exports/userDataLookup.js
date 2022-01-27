621dc416bf175ee29a77f92e0c1349e6d2a5a755
mrjohnming <john.ngai@hotmail.com>
2022-01-27 02:23:52 +0000

    Created a userURLsLookup function

--- exports/userDataLookup.js
+++ exports/userDataLookup.js
@@ -1,21 +1,6 @@
-/* Sample users object:
-const users = {
-  'userRandomID': {
-    id: 'userRandomID',
-    email: 'user@example.com',
-    password: 'purple-monkey-dinosaur'
-  },
-  'user2RandomID': {
-    id: 'user2RandomID',
-    email: 'user2@example.com',
-    password: 'dishwasher-funk',
-  },
-};
-*/
+// Helper functions for express_server.js
 
 
-
-// Helper function for express_server.js
 // Return true if the email param matches an email inside the users object.
 const emailLookup = (users, email) => {
   const userIDs = Object.keys(users);
@@ -27,7 +12,6 @@ const emailLookup = (users, email) => {
   return false;
 };
 
-// Helper function for express_server.js
 // Return true if an email & password combo matches a the values for a specific user inside the users object.
 const passwordLookup = (users, email, password) => {
   const userIDs = Object.keys(users);
@@ -39,7 +23,6 @@ const passwordLookup = (users, email, password) => {
   return false;
 };
 
-// Helper function for express_server.js
 // Return the matching user id for an email inside the users object.
 const userIDLookup = (users, email) => {
   const userIDs = Object.keys(users);
@@ -50,4 +33,15 @@ const userIDLookup = (users, email) => {
   }
 };
 
-module.exports = { emailLookup, passwordLookup, userIDLookup };
+// Return an object containing the urls for a specific user id.
+const userURLsLookup = (database, userID) => {
+  const myURLs = {};
+  for (const url in database) {
+    if (database[url]['userID'] === userID) {
+      myURLs[url] = {longURL: database[url]['longURL']};
+    }
+  }
+  return myURLs;
+};
+
+module.exports = { emailLookup, passwordLookup, userIDLookup, userURLsLookup };
